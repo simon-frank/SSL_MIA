44aea129dd952aafb5985254cd4ed167ad1ef1dc
OldFella <gwentkrause@gmail.com>
2023-06-01 12:03:26 +0200

    now converts all image to rgb when loading the data

--- utils/data.py
+++ utils/data.py
@@ -1,11 +1,16 @@
 from mimeta import MIMeta
 from lightly.data import LightlyDataset
 import torch
+from torchvision import transforms
 
 def get_data(config):
 
     generator = torch.Generator().manual_seed(config['seed'])
-    data = MIMeta(config['data'], config['domain'], config['task'])
+
+    make_rgb = transforms.Grayscale(num_output_channels = 3)
+
+    data = MIMeta(config['data'], config['domain'], config['task'], transform = make_rgb)
+
     litdata = LightlyDataset.from_torch_dataset(data, transform=config['transform'])
 
     data_splits = torch.utils.data.random_split(litdata, config['splits'], generator = generator)
