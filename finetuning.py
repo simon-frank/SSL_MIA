74f568398312fbfbb3bc6ae4c8e72a0359b0131c
Simon Frank <simon.frank98@web.de>
2023-06-04 10:53:16 +0200

    finetuning

--- finetuning.py
+++ finetuning.py
@@ -1,9 +1,41 @@
-
-
+from utils.modelFactory import createFinetuningModel
+from utils.data import get_data_pretraining, load_config, get_data_finetuning
+from utils.data import load_config
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+import torch
+import os 
 def main():
 
-    
-    pass
+    # Load config file
+    config = load_config('config.yaml')
+
+    # create model
+    model = createFinetuningModel(config)
+
+
+    train, val, test = get_data_finetuning(config)
+
+    save_path = os.path.join(config['savedmodel']['path'], config['savemodel']['name'])
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    checkpoint_callback = ModelCheckpoint(
+        dirpath=save_path)
+
+
+    dataloader = torch.utils.data.DataLoader(
+            train,
+            64,
+            shuffle = True)
+    accelerator = 'gpu' if torch.cuda.is_available() else 'cpu'
+    trainer = pl.Trainer(
+        max_epochs = config['epochs'],
+        devices='auto',
+        accelerator=accelerator,
+        callbacks=[checkpoint_callback],
+        log_every_n_steps=15,
+    )
+    trainer.fit(model= model, train_dataloaders=dataloader)
 
 if __name__ == '__main__':
     main()  
--- models/base.py
+++ models/base.py
@@ -1,5 +1,8 @@
 import torchvision.models as models
 from torch import nn
+import torch
+import pytorch_lightning as pl
+
 
 class Base50(nn.Module):
   def __init__(self, ReadoutHead):
@@ -22,3 +25,29 @@ class Base18(nn.Module):
     def forward(self,x):
         x = self.readoutHead(self.resnet18(x))
         return x
+    
+
+
+class Base(pl.LightningModule):
+  def __init__(self, backbone, ReadoutHead, config: dict):
+    super().__init__()
+    self.lr = config['finetuning']['lr']
+    self.optim = config['optimizer']
+    self.readoutHead = ReadoutHead
+    self.backbone = backbone
+    self.loss = nn.functional.cross_entropy
+
+  def forward(self, x):
+    x = self.readoutHead(self.backbone(x))
+    return x
+  
+  def training_step(self, batch, batch_index):
+    x,y = batch
+    y_hat = self.forward(x)
+    loss = self.loss(y_hat,y)
+    self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+    return loss
+  
+  def configure_optimizers(self):
+     optimizer = torch.optim.Adam(self.readoutHead.parameters(), lr=self.lr)
+     return optimizer
